976799d4f6a225e6f4ec6b9697a0b0b937121f69
Damien Bart <svusdroccksclub@gmail.com>
2019-08-24 21:47:50 +0200

    Hi

---
+++ Alien.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+import random
+class Alien(pygame.sprite.Sprite):
+    speed = 13
+    animation_cycle = 12
+    images = []
+
+    def _init_(self, screen_rectangle):
+        pygame.sprite.Sprite._init_(self,self.containers)
+        self.SCREENRECT = screen_rectangle
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        
