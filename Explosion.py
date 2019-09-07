1ab3ef71ee388d9b7b048744b16607108c4a5756
Damien Bart <svusdroccksclub@gmail.com>
2019-09-07 12:21:42 -0700

    Hi

--- Explosion.py
+++ Explosion.py
@@ -1,14 +1,11 @@
 import pygame
-from pygame.locals import *
-import random
-class Alien(pygame.sprite.Sprite):
-    speed = 13
-    animation_cycle = 12
+class Explosion(pygame.sprite.Sprite):
+    default_life = 12
+    animation_cycle = 3
     images = []
 
-    def _init_(self, screen_rectangle):
+    def _init_(self, actor):
         pygame.sprite.Sprite._init_(self,self.containers)
-        self.SCREENRECT = screen_rectangle
         self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.direction_factor = random.choice((-1, 1))
