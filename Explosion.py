c898101df1d8bcd281fbe3baa29b151231e4fa7c
Damien Bart <svusdroccksclub@gmail.com>
2019-09-07 12:27:53 -0700

    Hi

--- Explosion.py
+++ Explosion.py
@@ -7,19 +7,14 @@ class Explosion(pygame.sprite.Sprite):
     def _init_(self, actor):
         pygame.sprite.Sprite._init_(self,self.containers)
         self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.direction_factor = random.choice((-1, 1))
-        self.x_velocity = self. direction_factor * self.speed
-        self.frame = 0
-        if self.x_velocity < 0:
-            self.rect.right = self.SCREENRECT.right
-
-    def update(self):
-        self.rect.move_ip(self.x_velocity, 0)
-        if not self.SCREENRECT.contains(self.rect):
-            self.x_velocity = -self.x_velocity
-            self.rect.top = self.rect.bottom + 1
-            self.rect = self.rect.clamp(self.SCREENRECT)
-        self.frame = self.frame + 1
-        self.image = self.images[self.frame//self.animation_cycle % 3]
+        self.rect = self.image.get_rect(center=actor.rect.center)
+        self.life = self.default_life
         
+    def update(self):
+        '''
+            explosion will last 12 frames
+        '''
+        self.life = self.life - 1
+        self.image = self.images[self.life//self.animation_cycle % 2]
+        if (self.life <=0):
+            self.kill()
