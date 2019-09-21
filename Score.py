f5a99a9969822cb4df3895c22e730d27241f5125
Damien Bart <svusdroccksclub@gmail.com>
2019-09-21 13:01:18 -0700

    hi

---
+++ Score.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import Color
+class Score(pygame.sprite.Sprite):
+    score_points = 0
+    def _init_(self):
+        pygame.sprite.Sprite._init_(self)
+        self.font = pygame.font.Font(None, 48)
+        self.font.set_italic(1)
+        self.color = Color("White")
+        self.last_score = -1
+        self.update()
+        self.rect = self.image.get_rect().move(20,5)
+    def update(self):
+        if (self.score_points != self.last_score):
+            self.last_score = self.score_points
+            msg = "Score: %d" % self.score_points
+            self.image = self.font.render(msg, 0, self.color)
+            # line 29 for mr tim
