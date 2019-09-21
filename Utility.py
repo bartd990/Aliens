a9a03f704ed701d8b3ef502493f0934aeeeccdf3
Damien Bart <svusdroccksclub@gmail.com>
2019-09-21 12:33:33 -0700

    hi

---
+++ Utility.py
@@ -0,0 +1,16 @@
+import os.path
+import pygame
+main_directory = os.path.split(os.path.abspath(__file__))[0]
+def load_image(file):
+    file = os.path.join(main_directory, "Data", file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        raise SystemExit('Could not lot image')
+    return surface.convert()
+def load_images(*file_names):
+    images = []
+    for file_name in file_names:
+        image = load_image(file)
+        images.append(image)
+        
